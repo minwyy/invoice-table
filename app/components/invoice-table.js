20ec88d6f4638a68ec62af59b71796789a917813
Minwei Yao <dzrematt@gmail.com>
2020-10-28 02:37:05 +1100

    made a demo ember-table

--- app/components/invoice-table.js
+++ app/components/invoice-table.js
@@ -1,4 +1,37 @@
 import Component from '@glimmer/component';
+import { action, computed } from '@ember/object';
 
 export default class InvoiceTableComponent extends Component {
+    showSortIndicator= true;
+    showResizeHandle= true;
+
+    @action
+    changeSortIndicator () {
+        this.showSortIndicator = !this.showSortIndicator 
+    };
+
+    @action
+    changeResizeHandler () {
+        this.showResizeHandle = !this.showResizeHandle
+    };
+
+    @computed
+    get columns() {
+        return [
+            { name: 'A', valuePath: 'A', isFixed: 'left'},
+            { name: 'B', valuePath: 'B'},
+            { name: 'C', valuePath: 'C'},
+            { name: 'D', valuePath: 'D'},
+            { name: 'E', valuePath: 'E'},
+        ];
+      };
+    
+    @computed
+    get rows() {
+        return [
+            {A: "1", B: "ab", C: "ab", D: "cd", E: "de"},
+            {A: "1", B: "ab", C: "ab", D: "cd", E: "de"}
+        ];
+    }
+
 }
