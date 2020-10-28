59b04346dd8d96545dc7413dab64808a2e8c059b
Minwei Yao <dzrematt@gmail.com>
2020-10-28 18:25:15 +1100

    add sorting, filtering, and selection; works with mock data

--- app/components/invoice-table.js
+++ app/components/invoice-table.js
@@ -1,37 +1,76 @@
 import Component from '@glimmer/component';
 import { action, computed } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { mockData } from '../components/data';
 
 export default class InvoiceTableComponent extends Component {
-    showSortIndicator= true;
-    showResizeHandle= true;
+    // triggers for displaying selected items
+    @tracked showTransferIndicator = false;
+    @tracked showNotTransferIndicator = false;
+    @tracked showNotReconcileIndicator = false;
+    sorts = [];
+    selection = [];
 
     @action
-    changeSortIndicator () {
-        this.showSortIndicator = !this.showSortIndicator 
+    changeTransferIndicator () {
+        this.showTransferIndicator = true;
+        this.showNotTransferIndicator = false;
+        this.showNotReconcileIndicator = false;
     };
 
     @action
-    changeResizeHandler () {
-        this.showResizeHandle = !this.showResizeHandle
+    changeNotTransferIndicator () {
+        this.showNotTransferIndicator = true;
+        this.showTransferIndicator = false;
+        this.showNotReconcileIndicator = false;
     };
+
+    @action
+    changeNotReconcileIndicator () {
+        this.showNotReconcileIndicator = true;
+        this.showTransferIndicator = false;
+        this.showNotTransferIndicator = false;
+    };
+
+    @action
+    changeAllIndicator () {
+        this.showTransferIndicator = false;
+        this.showNotTransferIndicator = false;
+        this.showNotReconcileIndicator = false;
+    };
+
 
     @computed
     get columns() {
         return [
-            { name: 'A', valuePath: 'A', isFixed: 'left'},
-            { name: 'B', valuePath: 'B'},
-            { name: 'C', valuePath: 'C'},
-            { name: 'D', valuePath: 'D'},
-            { name: 'E', valuePath: 'E'},
+            { name: 'Invoice No.', valuePath: 'invoiceNo', isFixed: 'left'},
+            { name: 'Client', valuePath: 'client'},
+            { name: 'Invoice Date', valuePath: 'invoiceDate'},
+            { name: 'Gross', valuePath: 'gross'},
+            { name: 'Tax', valuePath: 'tax'},
+            { name: 'Total Inc. Tax', valuePath: 'totalInclueTax'},
+            { name: 'Transferred', valuePath: 'transferred'},
+            { name: 'Reconciled', valuePath: 'reconciled'},
+            { name: 'Accounts', valuePath: 'accounts'}
         ];
       };
     
-    @computed
+    @computed('showTransferIndicator')
     get rows() {
-        return [
-            {A: "1", B: "ab", C: "ab", D: "cd", E: "de"},
-            {A: "1", B: "ab", C: "ab", D: "cd", E: "de"}
-        ];
-    }
-
+        let data = {...mockData};
+        let t = ["True"];
+        let f = ["False"];
+        // filter transfered invoice
+        if (this.showTransferIndicator) {
+            data.rows = data.rows.filter( i => t.includes( i.transferred ))
+            return data.rows
+        } else if (this.showNotTransferIndicator) {
+            data.rows = data.rows.filter( i => f.includes( i.transferred ))
+            return data.rows
+        } else if (this.showNotReconcileIndicator) {
+            data.rows = data.rows.filter( i => f.includes( i.reconciled ))
+            return data.rows
+        }
+            return mockData.rows
+        }
 }
